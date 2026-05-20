9e2fbd86fe89344ae3e416d5f13072bd8718f721
soulaiman <84672766+solaymane403@users.noreply.github.com>
2026-05-20 07:55:19 +0100

    new done

--- DAYS/C01/ex07/main.c
+++ DAYS/C01/ex07/main.c
@@ -4,9 +4,9 @@ void	ft_rev_int_tab(int *tab, int size);
 
 int main (void)
 {
-    int arr[] = {1,2,3,4,5};
+    int arr[] = {1,2,3,4};
     int i = 0;
-    int size = 5;
+    int size = 4;
 
     ft_rev_int_tab( arr , size);
 
